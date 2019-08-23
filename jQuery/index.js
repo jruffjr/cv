f3564c9b5d036870400c7631810f467812ce30ba
jruffjr <54455028+jruffjr@users.noreply.github.com>
2019-08-23 16:48:12 -0400

    Add website files

---
+++ jQuery/index.js
@@ -0,0 +1,30 @@
+/*to change text use .text
+$=jQuery
+attributes are $("a").attr("href","https://www.yahoo.com")
+Event listeners:
+$() is the jQuery selector
+javascript commands:
+.toUpperCase()
+.toLowerCase()
+var myName = "name";
+function getmilk(input) {}
+to call the function getmilk();
+word.length
+Math.floor(round down number)
+Modulo % tells you remainder of division
+Random Number Generation var n = Math.random()
+=== is equal to, checks for same datatypes
+!== is not equal
+>= is greater or equal
+<= is lesser or equal to
+== datatypes do not matter
+arrays look like this:  var x = ["it0","it1","it2","it3"];
+var item 1 = x{it1}
+x.length; counts number of items in array
+x.includes() checks if item inside parenthesis is in array
+eggs.push puts items on the end of the array
+eggs.pop takes away the last item in the array
+order matters in if and else statements*/
+alert("hello")
+$("h1").RemoveClass("b-title margin-10");
+function doStuff(){}
